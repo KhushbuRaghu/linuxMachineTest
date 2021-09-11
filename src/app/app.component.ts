b451b29c94d1f1eccd1234ef4677d43fbb34dbc9
khushburaghu <khushi.raghu.kr@gmail.com>
2021-09-11 15:03:13 +0530

    create registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simple-machine-test';
+  public form?: FormGroup | any;
+  public userList: any = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.createForm();
+  }
+
+  public add(): void {
+    const control = <FormArray>this.form.controls['user_detail'];
+    control.push(this.getControls());
+  }
+
+  public getForm(): any {
+    return (this.form.get('user_detail') as FormArray).controls;
+  }
+
+  public remove(i: number) {
+    const contrls = <FormArray>this.form.controls['user_detail'];
+    contrls.removeAt(i);
+  }
+
+  public submit(): void {
+    this.userList = this.form.value.user_detail;
+    this.form.reset();
+    this.createForm();
+
+  }
+
+  private createForm(): void {
+    this.form = this.formBuilder.group({
+      user_detail: this.formBuilder.array([this.getControls()]),
+    });
+  }
+
+  private getControls(): any {
+    return this.formBuilder.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+    });
+  }
 }
